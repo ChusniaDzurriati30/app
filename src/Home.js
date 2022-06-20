921fb861225e8a3f9947015d10a713e97acf4659
ChusniaDzurriati30 <107242177+ChusniaDzurriati30@users.noreply.github.com>
2022-06-20 17:42:53 +0700

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -121,12 +121,19 @@ export default function Example() {
               </p>
               <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                 <div className="rounded-md shadow">
-                  <a
-                    href="#"
-                    className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-yellow-400 bg-white hover:bg-yellow-200 md:py-4 md:text-lg md:px-10"
+                 <a
+                    href="./ContactPerson"
+                    className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-yellow-400 bg-white hover:bg-yellow-700 md:py-4 md:text-lg md:px-10"
                   >
-                    Order Now 
-                    
+                    Pesan 
+                  </a>
+                </div>
+                <div className="mt-3 sm:mt-0 sm:ml-3">
+                  <a
+                    href="./Promo"
+                    className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-yellow-400 bg-red-700 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"
+                  >
+                    Cek Promo
                   </a>
                 </div>
               </div>
